e780893af17881ce6c378b0074cf3c27a02c2843
licat <licat233@gmail.com>
2023-05-11 12:20:44 +0800

    完善model core代码；完善更新升级代码；

--- update/update.go
+++ update/update.go
@@ -64,55 +64,43 @@ func updateSelf(latestVersion string) error {
 }
 
 func compareVersions(oldVer, newVer string) bool {
-	oldParts := strings.Split(oldVer, ".")
-	newParts := strings.Split(newVer, ".")
+	oldVer, newVer = strings.TrimSpace(oldVer), strings.TrimSpace(newVer)
+	oldLen, newLen := len(oldVer), len(newVer)
+	minLen := oldLen
+	if oldLen > newLen {
+		minLen = newLen
+	}
 
-	for i := 0; i < len(newParts) && i < len(oldParts); i++ {
-		oldNum, oldStr := parseVersionPart(oldParts[i])
-		newNum, newStr := parseVersionPart(newParts[i])
-
-		if newNum > oldNum {
+	var oldVerScore, newVerScore int
+	for i := 0; i < minLen; i++ {
+		oldV, newV := oldVer[i], newVer[i]
+		if oldV == newV {
+			continue
+		}
+		oldDigit, oldErr := strconv.Atoi(string(oldV))
+		newDigit, newErr := strconv.Atoi(string(newV))
+		if oldErr != nil && newErr == nil {
 			return true
-		} else if newNum < oldNum {
+		}
+		if newErr != nil && oldErr == nil {
 			return false
 		}
-
-		// If both parts are equal, but one has a suffix and the other doesn't,
-		// consider the one without the suffix to be newer.
-		if oldStr == "" && newStr != "" {
+		if oldErr != nil && newErr != nil {
 			return true
-		} else if oldStr != "" && newStr == "" {
-			return false
+		}
+		if oldErr == nil && newErr == nil {
+			if newDigit > oldDigit {
+				newVerScore++
+			} else if newDigit < oldDigit {
+				oldVerScore++
+			} else {
+				continue
+			}
 		}
 	}
-
-	// If all parts match, but the second version has more parts, it's considered newer.
-	return len(newParts) > len(oldParts)
-}
-
-// Parses a single part of a version number, which may contain numbers and/or letters.
-// Returns the numerical part as an integer, and the non-numerical part as a string.
-func parseVersionPart(part string) (int, string) {
-	dotIdx := strings.IndexByte(part, '.')
-	if dotIdx >= 0 {
-		num, _ := strconv.Atoi(part[0:dotIdx])
-		return num, part[dotIdx+1:]
+	if oldVerScore == newVerScore {
+		return oldLen > newLen
 	}
 
-	num, err := strconv.Atoi(part)
-	if err == nil {
-		// If the whole part is a number, return it as-is.
-		return num, ""
-	}
-
-	// Otherwise, split the part into numeric and non-numeric parts.
-	for i := len(part); i > 0; i-- {
-		s := part[:i]
-		if digit, err := strconv.Atoi(s); err == nil {
-			return digit, ""
-		}
-	}
-
-	// If there are no numeric characters in the part, treat it as 0.
-	return 0, part
+	return oldVerScore < newVerScore
 }
